0dc448abfb4d42415b65067e0f0206e3f0eb5431
William Guilherme <wguilherme@securitygeek.io>
2022-01-09 14:17:12 -0800

    updated html_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,9 +30,9 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx_rtd_theme"
-]
+# extensions = [
+#     "furo"
+# ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,7 +48,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 
 html_context = {
     "display_github": True,
