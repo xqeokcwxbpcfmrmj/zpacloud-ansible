cf9246022d5cb8c6cfb3fc930cdce58b9788a561
William Guilherme <wguilherme@securitygeek.io>
2022-02-21 15:10:44 -0800

    Fix: ``make check-format`` and ``make format``

--- tests/unit/plugins/module_utils/test_zpa_app_connector_group.py
+++ tests/unit/plugins/module_utils/test_zpa_app_connector_group.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import, division, print_function
 
-
 __metaclass__ = type
 
 import unittest
 from unittest.mock import MagicMock
+
 from ansible_collections.willguibr.zpacloud.plugins.module_utils.zpa_app_connector_group import (
     AppConnectorGroupService,
 )
@@ -16,13 +16,16 @@ class TestAppConnectorGroupService(unittest.TestCase):
         rest = MagicMock()
         rest.get = MagicMock()
         rest.get.return_value.status_code = 200
-        rest.get.return_value.json = {"name": "bar", "id": "test", "connectors": [{
-            "someField": "value"
-        }]}
+        rest.get.return_value.json = {
+            "name": "bar",
+            "id": "test",
+            "connectors": [{"someField": "value"}],
+        }
         k = AppConnectorGroupService(module, "", rest)
-        self.assertEqual(k.getByID("test"), {"name": "bar", "id": "test", "connectors": [{
-            "some_field": "value"
-        }]})
+        self.assertEqual(
+            k.getByID("test"),
+            {"name": "bar", "id": "test", "connectors": [{"some_field": "value"}]},
+        )
 
     def test_get_by_id_when_nok(self):
         module = MagicMock()
@@ -37,35 +40,13 @@ class TestAppConnectorGroupService(unittest.TestCase):
         rest = MagicMock()
         rest.get_paginated_data = MagicMock()
         rest.get_paginated_data.return_value = [
-            {
-                "name": "bar1",
-                "id": "test1",
-                "connectors": [
-                        {
-                            "someField": "value1"
-                        }
-                ]
-            },
-            {
-                "name": "bar2",
-                "id": "test2",
-                "connectors": [
-                        {
-                            "someField": "value2"
-                        }
-                ]
-            }
+            {"name": "bar1", "id": "test1", "connectors": [{"someField": "value1"}]},
+            {"name": "bar2", "id": "test2", "connectors": [{"someField": "value2"}]},
         ]
         k = AppConnectorGroupService(module, "", rest)
-        self.assertEqual(k.getByName("bar2"), {
-            "name": "bar2",
-            "id": "test2",
-            "connectors": [
-                {
-                    "some_field": "value2"
-                }
-            ]
-        }
+        self.assertEqual(
+            k.getByName("bar2"),
+            {"name": "bar2", "id": "test2", "connectors": [{"some_field": "value2"}]},
         )
 
     def test_get_by_name_when_nok(self):
@@ -77,12 +58,8 @@ class TestAppConnectorGroupService(unittest.TestCase):
         self.assertIsNone(k.getByName("test"))
 
     def test_create_when_ok(self):
-        jsonObj = {"name": "bar", "id": "test", "connectors": [{
-            "someField": "value"
-        }]}
-        obj = {"name": "bar", "id": "test", "connectors": [{
-            "some_field": "value"
-        }]}
+        jsonObj = {"name": "bar", "id": "test", "connectors": [{"someField": "value"}]}
+        obj = {"name": "bar", "id": "test", "connectors": [{"some_field": "value"}]}
         module = MagicMock()
         rest = MagicMock()
         rest.post = MagicMock()
@@ -97,17 +74,15 @@ class TestAppConnectorGroupService(unittest.TestCase):
         rest.post = MagicMock()
         rest.post.return_value.status_code = 400
         k = AppConnectorGroupService(module, "", rest)
-        self.assertIsNone(k.create({"name": "bar", "id": "test", "connectors": [{
-            "someField": "value"
-        }]}))
+        self.assertIsNone(
+            k.create(
+                {"name": "bar", "id": "test", "connectors": [{"someField": "value"}]}
+            )
+        )
 
     def test_update_when_ok(self):
-        jsonObj = {"name": "bar", "id": "test", "connectors": [{
-            "someField": "value"
-        }]}
-        obj = {"name": "bar", "id": "test", "connectors": [{
-            "some_field": "value"
-        }]}
+        jsonObj = {"name": "bar", "id": "test", "connectors": [{"someField": "value"}]}
+        obj = {"name": "bar", "id": "test", "connectors": [{"some_field": "value"}]}
         module = MagicMock()
         rest = MagicMock()
         rest.put = MagicMock()
@@ -125,6 +100,8 @@ class TestAppConnectorGroupService(unittest.TestCase):
         rest.put = MagicMock()
         rest.put.return_value.status_code = 400
         k = AppConnectorGroupService(module, "", rest)
-        self.assertIsNone(k.update({"name": "bar", "id": "test", "connectors": [{
-            "someField": "value"
-        }]}))
+        self.assertIsNone(
+            k.update(
+                {"name": "bar", "id": "test", "connectors": [{"someField": "value"}]}
+            )
+        )
